6da564cb4c9ef11f74bfffd9e1a445088a0c8d7d
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2022-11-11 10:37:13 -0500

    userenterprise

--- routes/usersEnterprise.routes.js
+++ routes/usersEnterprise.routes.js
@@ -8,7 +8,7 @@ const {
   //protectEmployee,
   protectAccountOwner,
 } = require('../middlewares/usersenterprises.middlewares');
-
+//
 const {
   createUserEnterpriseValidations,
   checkValidations,
